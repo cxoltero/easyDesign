6769adce5c8d81e8c0579e28e7109acd38ef5260
cxoltero <carlosh95@gmail.com>
2014-11-20 21:20:58 -0500

    added extra fields to user migration

--- db/migrate/20141121020740_devise_create_users.rb
+++ db/migrate/20141121020740_devise_create_users.rb
@@ -30,6 +30,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # extra info
+      t.string :username
+      t.string :name
+      t.string :lastname
+
 
       t.timestamps
     end
